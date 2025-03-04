ec791f886f47591331c0aabd792d608aa006668c
Mahdi <mahdibagherichanel@gmail.com>
2025-03-04 14:11:41 +0330

    feat: enhance ListHeader component with dynamic menu items and icons

--- src/components/Header/ListHeader.tsx
+++ src/components/Header/ListHeader.tsx
@@ -1,5 +1,33 @@
+import { FaAngleDown } from 'react-icons/fa6'
+
+const itemHeader = {
+  name: 'تماس با ما',
+  name1: 'درباره ما',
+  name2: 'اعطای نمایندگی',
+  name3: { label: 'منو', icon: <FaAngleDown /> },
+  name4: { label: 'شعبه', icon: <FaAngleDown /> },
+  name5: 'صفحه اصلی',
+}
+
 function ListHeader() {
-  return <div>ListHeader</div>
+  return (
+    <div className="flex gap-4">
+      {Object.values(itemHeader).map((item, index) => (
+        <div key={index} className="flex items-center">
+          {typeof item === 'object' ? (
+            <>
+              <div className="flex items-center justify-center">
+                <span>{item.icon}</span>
+                <span>{item.label}</span>
+              </div>
+            </>
+          ) : (
+            <span>{item}</span>
+          )}
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default ListHeader
