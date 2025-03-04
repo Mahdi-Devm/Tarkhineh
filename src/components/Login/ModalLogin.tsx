7b712c8da1a2aa521de1ec67b6924cc5e04176dd
Mahdi <mahdibagherichanel@gmail.com>
2025-03-04 19:53:45 +0330

    feat: enhance ModalLogin component with logo and user input fields

--- src/components/Login/ModalLogin.tsx
+++ src/components/Login/ModalLogin.tsx
@@ -1,3 +1,6 @@
+import { IoIosClose } from 'react-icons/io'
+import Logo from '../Logo/Logo'
+
 interface ModalProps {
   isMobile: boolean
   isModalOpen: boolean
@@ -19,13 +22,23 @@ const Modal: React.FC<ModalProps> = ({
           : 'top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform'
       } z-50 flex items-center justify-center transition-all duration-300`}
     >
-      <div className="w-[90%] rounded-lg bg-white p-4 shadow-lg md:w-[400px]">
+      <div className="relative w-[90%] rounded-lg bg-white p-4 shadow-lg md:w-[400px]">
         <h2>مدال</h2>
+        <div>
+          <div>
+            <Logo />
+          </div>
+          <p>ورود / ثبت ‌نام</p>
+          <p>با وارد کردن شماره موبایل کد تاییدی برای شما ارسال خواهد شد.</p>
+          <input type="text" placeholder="شماره تلفن" />
+          <button>ادامه</button>
+          <p>ورود و عضویت در ترخینه به منزله قبول قوانین و مقررات است.</p>
+        </div>
         <button
           onClick={toggleModal}
-          className="mt-4 text-red-500 hover:text-red-700"
+          className="absolute top-2 right-2 p-2 text-gray-500 hover:text-gray-700"
         >
-          بستن
+          <IoIosClose className="h-[24px] w-[24px]" />
         </button>
       </div>
     </div>
