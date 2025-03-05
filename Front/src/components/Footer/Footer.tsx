aac7d04bbc67573eb8efa40ce387f235e23798f1
Mahdi <mahdibagherichanel@gmail.com>
2025-03-05 19:22:47 +0330

    feat: update Footer component with background overlay and layout adjustments

--- Front/src/components/Footer/Footer.tsx
+++ Front/src/components/Footer/Footer.tsx
@@ -4,17 +4,18 @@ import FooterBranches from './FooterBranches'
 import FooterSocials from './FooterSocials'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-
 function Footer() {
   return (
     <div
-      className="w-full bg-cover bg-center px-10 py-0 text-white sm:px-15 sm:py-4"
+      className="relative w-full bg-cover bg-center px-10 py-0 text-white sm:px-15 sm:py-4"
       style={{
         backgroundImage:
           'url(https://s3-alpha-sig.figma.com/img/85b4/9d91/9346f0ad088135a337fb916e224331cf?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=QsufMwy1UAx1D0yDZTe5VcNTKcWqlydWXy8MaFHYSSyfoEgu46Y-mDix2VicDMlA871kZR7gCABFCHHzVjTJ-1vYwzCwfjTRb28i-SvozfIUDy8Xec1BhM48W1AST7hfr9U4dpy~BLrUwuzfwMlOLcKDlBl~uyum5rluYequwZyYPv2GA7HGFpUIIFoPDMSw4-l-3XmFaxNWdph9GV27jLM-4kKBkI97YpsfRXiuJjyw2JvgX7A0VMU9tqLDklgGRbXdpe4pXAtyNbglbJgJLUNzwXBOOPMDhL022KYiA-tfRr-jAQCLC9a7DTx7MuG~nCd8tXZ-UxU4eHiLhTAYxw__)',
       }}
     >
-      <div className="mx-auto flex max-w-screen-xl flex-col gap-10 md:flex-row">
+      <div className="absolute inset-0 bg-stone-950 opacity-30 mix-blend-multiply"></div>
+
+      <div className="relative mx-auto flex max-w-screen-xl flex-col gap-10 md:flex-row">
         <div className="hidden w-full md:block md:w-[85%]">
           <h3 className="mb-3 text-right text-2xl font-bold">پیام به ترخینه</h3>
           <FooterForm />
