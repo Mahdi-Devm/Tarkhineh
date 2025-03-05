a9f0964b83697538b05d1647ee285e683d16a85f
Mahdi <mahdibagherichanel@gmail.com>
2025-03-05 14:50:37 +0330

    feat: enhance Footer component with improved form handling, styling, and accessibility features

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,37 +1,43 @@
 import { CiInstagram, CiTwitter, CiLinkedin } from 'react-icons/ci'
 
 function Footer() {
+  function handelclick(e: React.MouseEvent<HTMLFormElement, MouseEvent>) {
+    e.preventDefault()
+  }
   return (
     <div className="w-full bg-[url(https://s3-alpha-sig.figma.com/img/85b4/9d91/9346f0ad088135a337fb916e224331cf?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=QsufMwy1UAx1D0yDZTe5VcNTKcWqlydWXy8MaFHYSSyfoEgu46Y-mDix2VicDMlA871kZR7gCABFCHHzVjTJ-1vYwzCwfjTRb28i-SvozfIUDy8Xec1BhM48W1AST7hfr9U4dpy~BLrUwuzfwMlOLcKDlBl~uyum5rluYequwZyYPv2GA7HGFpUIIFoPDMSw4-l-3XmFaxNWdph9GV27jLM-4kKBkI97YpsfRXiuJjyw2JvgX7A0VMU9tqLDklgGRbXdpe4pXAtyNbglbJgJLUNzwXBOOPMDhL022KYiA-tfRr-jAQCLC9a7DTx7MuG~nCd8tXZ-UxU4eHiLhTAYxw__)] bg-cover bg-center px-15 py-20 text-white sm:px-10">
       <div className="flex flex-col items-start justify-between gap-10 md:flex-row">
         <div className="h-[204px] w-150">
           <h3 className="mb-3 text-right text-2xl font-bold">پیام به ترخینه</h3>
-          <form className="space-y-">
+          <form className="space-y-4" onClick={handelclick}>
             <div className="flex gap-3">
               <textarea
                 placeholder="پیام شما"
-                className="w-full rounded-lg border border-gray-600 p-2 text-right text-white placeholder-gray-400 focus:ring-2 focus:ring-orange-500"
+                className="w-full rounded-lg border border-gray-600 p-2 text-right text-white placeholder-gray-400 transition duration-300 ease-in-out focus:text-[#417F56] focus:ring-2 focus:outline-none"
               />
 
               <div className="flex h-[144px] w-[275px] flex-col gap-[12px]">
                 <input
                   type="text"
                   placeholder="نام و نام خانوادگی"
-                  className="h-[40px] w-[276px] rounded-lg border border-gray-600 p-2 text-right text-white placeholder-gray-400 focus:ring-2 focus:ring-orange-500"
+                  className="h-[40px] w-[276px] rounded-lg border border-gray-600 p-2 text-right text-white placeholder-gray-400 transition duration-300 ease-in-out focus:text-[#417F56] focus:ring-2 focus:outline-none"
                 />
                 <input
                   type="text"
                   placeholder="شماره تماس"
-                  className="h-[40px] w-[276px] rounded-lg border border-gray-600 p-2 text-right text-white placeholder-gray-400 focus:ring-2 focus:ring-orange-500"
+                  className="h-[40px] w-[276px] rounded-lg border border-gray-600 p-2 text-right text-white placeholder-gray-400 transition duration-300 ease-in-out focus:text-[#417F56] focus:ring-2 focus:outline-none"
                 />
                 <input
                   type="text"
                   placeholder="آدرس ایمیل (اختیاری)"
-                  className="h-[40px] w-[276px] rounded-lg border border-gray-600 p-2 text-right text-white placeholder-gray-400 focus:ring-2 focus:ring-orange-500"
+                  className="h-[40px] w-[276px] rounded-lg border border-gray-600 p-2 text-right text-white placeholder-gray-400 transition duration-300 ease-in-out focus:text-[#417F56] focus:ring-2 focus:outline-none"
                 />
               </div>
             </div>
-            <button className="mt-1 h-[40px] w-[183px] rounded-lg border-1 border-gray-600 py-2 text-white transition">
+            <button
+              type="submit"
+              className="mt-1 h-[40px] w-[183px] rounded-lg border-1 border-gray-600 py-2 text-white transition duration-300 ease-in-out hover:border-[#417F56]"
+            >
               ارسال پیام
             </button>
           </form>
