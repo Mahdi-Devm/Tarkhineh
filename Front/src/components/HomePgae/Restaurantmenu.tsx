3fd9f5adaf39df3a6e734e990491aaf2d19385b7
Mahdi <mahdibagherichanel@gmail.com>
2025-03-06 09:47:33 +0330

    style: update Restaurantmenu component layout and styling

--- Front/src/components/HomePgae/Restaurantmenu.tsx
+++ Front/src/components/HomePgae/Restaurantmenu.tsx
@@ -2,24 +2,28 @@ import { images } from '../../constants/ItemMenuRes'
 
 function Restaurantmenu() {
   return (
-    <div className="relative flex h-[400px] items-center justify-center overflow-hidden">
-      <div className="absolute top-0 left-0 h-1/2 w-full"></div>
-
+    <div className="relative flex h-[500px] items-center justify-center">
       <div className="relative z-10 flex flex-col items-center">
-        <h1 className="mb-4 text-center text-2xl font-semibold text-[rgba(53,53,53,1)]">
+        <h1 className="mb-6 text-center text-3xl font-bold text-[rgba(53,53,53,1)]">
           منوی رستوران
         </h1>
-        <div className="flex gap-4">
+        <div className="mt-25 flex flex-wrap justify-center gap-8">
           {images.map((img, index) => (
             <div
               key={index}
-              className="relative h-[160px] w-[287.5px] overflow-hidden rounded-lg bg-white shadow-lg"
+              className="relative flex h-[220px] w-[280px] flex-col items-center justify-center rounded-lg bg-[#417F56] shadow-xl"
             >
               <img
                 src={img}
                 alt={`Food item ${index + 1}`}
-                className="absolute -top-8 left-1/2 h-auto w-[180px] -translate-x-1/2 transform object-cover"
+                className={`absolute left-1/2 z-20 -translate-x-1/2 transform object-cover transition-all duration-300 ${
+                  index === 0 ? '-top-40 w-[260px]' : '-top-25 w-[260px]'
+                }`}
               />
+
+              <div className="absolute bottom-2 h-[48px] w-[155px] rounded-[4px] bg-[#F9F9F9] p-[8px] text-center text-sm font-medium text-gray-700">
+                توضیحات محصول
+              </div>
             </div>
           ))}
         </div>
