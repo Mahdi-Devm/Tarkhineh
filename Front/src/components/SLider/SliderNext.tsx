964a608f1a4ae0b50b6dcc5e41308cf3f9fd83f6
Mahdi <mahdibagherichanel@gmail.com>
2025-03-06 08:52:27 +0330

    Slider Css

--- Front/src/components/SLider/SliderNext.tsx
+++ Front/src/components/SLider/SliderNext.tsx
@@ -1,25 +1,22 @@
 import React, { useState } from 'react'
-import { FaArrowLeft, FaArrowRight } from 'react-icons/fa' // استفاده از آیکون‌های react-icons
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 
-// تصاویر
 const images: string[] = [
-  'https://i.imgur.com/Gu5Cznz.jpg', // bogliasco
-  'https://i.imgur.com/idjXzVQ.jpg', // countyClare
-  'https://i.imgur.com/8DYumaY.jpg', // craterRock
-  'https://i.imgur.com/8IuucQZ.jpg', // giauPass
+  'https://i.imgur.com/Gu5Cznz.jpg',
+  'https://i.imgur.com/idjXzVQ.jpg',
+  'https://i.imgur.com/8DYumaY.jpg',
+  'https://i.imgur.com/8IuucQZ.jpg',
 ]
 
 const SimpleSlider: React.FC = () => {
-  const [currentIndex, setCurrentIndex] = useState<number>(0) // state برای نگهداری اندیس تصویر فعلی
+  const [currentIndex, setCurrentIndex] = useState<number>(0)
 
-  // تابع برای رفتن به تصویر قبلی
   const goToPrevious = () => {
     setCurrentIndex((prevIndex) =>
       prevIndex === 0 ? images.length - 1 : prevIndex - 1,
     )
   }
 
-  // تابع برای رفتن به تصویر بعدی
   const goToNext = () => {
     setCurrentIndex((prevIndex) =>
       prevIndex === images.length - 1 ? 0 : prevIndex + 1,
@@ -27,29 +24,40 @@ const SimpleSlider: React.FC = () => {
   }
 
   return (
-    <div className="relative mx-auto flex w-full max-w-[800px] items-center justify-center">
-      {/* دکمه قبلی */}
-      <button
-        onClick={goToPrevious}
-        className="absolute top-1/2 left-0 flex -translate-y-1/2 transform cursor-pointer items-center justify-center rounded-full border-none bg-black/50 p-2.5 text-2xl text-white"
-      >
-        <FaArrowLeft />
-      </button>
+    <div className="relative mx-auto flex w-full max-w-[1400px] flex-col items-center justify-center">
+      <div className="relative flex h-[600px] w-full items-center justify-center overflow-hidden rounded-lg shadow-lg">
+        <button
+          onClick={goToPrevious}
+          className="absolute top-1/2 left-4 z-10 flex -translate-y-1/2 transform cursor-pointer items-center justify-center rounded-full border-none bg-black/50 p-3 text-2xl text-white transition-all hover:bg-black/70"
+        >
+          <FaArrowLeft />
+        </button>
 
-      {/* نمایش تصویر فعلی */}
-      <img
-        src={images[currentIndex]}
-        alt={`Slide ${currentIndex}`}
-        className="h-auto w-full rounded-lg shadow-lg"
-      />
+        <img
+          src={images[currentIndex]}
+          alt={`Slide ${currentIndex}`}
+          className="h-full w-full object-cover"
+        />
 
-      {/* دکمه بعدی */}
-      <button
-        onClick={goToNext}
-        className="absolute top-1/2 right-0 flex -translate-y-1/2 transform cursor-pointer items-center justify-center rounded-full border-none bg-black/50 p-2.5 text-2xl text-white"
-      >
-        <FaArrowRight />
-      </button>
+        <button
+          onClick={goToNext}
+          className="absolute top-1/2 right-4 z-10 flex -translate-y-1/2 transform cursor-pointer items-center justify-center rounded-full border-none bg-black/50 p-3 text-2xl text-white transition-all hover:bg-black/70"
+        >
+          <FaArrowRight />
+        </button>
+      </div>
+
+      <div className="mt-4 flex space-x-2">
+        {images.map((_, index) => (
+          <div
+            key={index}
+            onClick={() => setCurrentIndex(index)}
+            className={`h-3 w-3 cursor-pointer rounded-full transition-all ${
+              index === currentIndex ? 'bg-blue-500' : 'bg-gray-300'
+            }`}
+          />
+        ))}
+      </div>
     </div>
   )
 }
