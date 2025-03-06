0c159531ceb32aebf2324fe385b77d369302ae27
Mahdi <mahdibagherichanel@gmail.com>
2025-03-06 09:37:28 +0330

    Add restaurant menu images and update image import structure

--- Front/src/components/HomePgae/Restaurantmenu.tsx
+++ Front/src/components/HomePgae/Restaurantmenu.tsx
@@ -1,25 +1,27 @@
-import { images } from '../../constants/ItemSlider'
+import { images } from '../../constants/ItemMenuRes'
 
 function Restaurantmenu() {
   return (
-    <div className="flex items-center justify-center">
-      <div className="flex flex-col">
-        <h1 className="text-[rgba(53, 53, 53, 1)] text-center text-2xl font-semibold">
-          منوی رستوران{' '}
+    <div className="relative flex h-[400px] items-center justify-center overflow-hidden">
+      <div className="absolute top-0 left-0 h-1/2 w-full"></div>
+
+      <div className="relative z-10 flex flex-col items-center">
+        <h1 className="mb-4 text-center text-2xl font-semibold text-[rgba(53,53,53,1)]">
+          منوی رستوران
         </h1>
-        <div className="flex gap-2">
-          <div className="bg-[rgba(65, 127, 86, 1)] h-[160px] w-[287.5px]">
-            <img src={images} alt="" />
-          </div>
-          <div className="bg-[rgba(65, 127, 86, 1)] h-[160px] w-[287.5px]">
-            <img src={images} alt="" />
-          </div>
-          <div className="bg-[rgba(65, 127, 86, 1)] h-[160px] w-[287.5px]">
-            <img src={images} alt="" />
-          </div>
-          <div className="bg-[rgba(65, 127, 86, 1)] h-[160px] w-[287.5px]">
-            <img src={images} alt="" />
-          </div>
+        <div className="flex gap-4">
+          {images.map((img, index) => (
+            <div
+              key={index}
+              className="relative h-[160px] w-[287.5px] overflow-hidden rounded-lg bg-white shadow-lg"
+            >
+              <img
+                src={img}
+                alt={`Food item ${index + 1}`}
+                className="absolute -top-8 left-1/2 h-auto w-[180px] -translate-x-1/2 transform object-cover"
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- Front/src/constants/ItemMenuRes.tsx
+++ Front/src/constants/ItemMenuRes.tsx
@@ -1,6 +1,6 @@
-export const images: string[] = [
-  'https://s3-alpha-sig.figma.com/img/a10c/16e3/62b32bed5e9795da0f85b05191645525?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=E59NmE8tnR3wQUfI3CeEBOEJUQUMXDm-Mv0Rvm8a4vskSDGpJ1dUWJl8bpLCwwlUSEuHsQTtAOZcgCV-Do2nsaHHKV3ejunZ2UVWwe-ggFjH02wx9DT4rpO0zjbPLXGmtCgAjgbnqjbxrXvXe08zQFnrWfJHtk-BcNw84mr3EzTCCfxvHM~mFc4txyfvJhcUTJKQUMHR-oFeJwX-BCZ6jaU1Z5KzdVs~cLTX2oY4hoDBzmdrr9EGkyXR4SZw0nXXoOiy0KTAjpSAAaCXJA4SSwQjnYN6dEf-~LNWt3axaESNflHiQPEO81x0NpNvhFUgnIEvSCjG2w9ipMAwFvX-gQ__',
-  'https://s3-alpha-sig.figma.com/img/c5cc/9edd/e5b7a07a034333adda7b110c42ad323b?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=Y99htVRVjKHcXmAKhGgtdvNCfFdBNDoREz7i1RtBOhXy1oQe8wwzRiX-QkYGIWmJmJGWuHYfuS0c0pld8e27FfIXTPNlmbQGUTZSTAvOl0y0XdyjjZwQNW4KSZvBO5aQdT5ZjBzRgLYO4qgoDwEycLsY7s4ImBeUdsFXRxCghJgvpLoWmmygjcxDOyu2NIpQdWhe554wQz0t1qZLkRqyrsYZGJl3jUIAA8yvcqL2hMqmoU-l03VSltxv~uaCP8clCkPmHcaKBtXjczvbnSrB72ENw2~Zho21LhOQo1wsdwgb~l3UvX2N4l3RRSHZyXmtQJPOJJzhzkWpf5zfNqesbg__',
-  'https://s3-alpha-sig.figma.com/img/603a/9666/60b66f76b449e958ae4f1299284b4558?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=kxJq3ifxUwzCp2a0dPq00jX7tno7i34YgT0Ums-wlONymOsD-Z90tk9x-6MS7Ixy82o5usQNLZAX4~ZUsWGq7D1WMHBC7gdR5Z0j-CdBHbyJByWqMWJpVg5zhb1SHUkwqugjbpFEaQ9nRD1QRDzGDgFXfULsCX91olcGFycpmGB8cA0Ez4MsHnqAOVCqIr26lwbH5u-5WEqseIKXITNuOZ3Fsfgq-VJxLjN1kLf3dFgdeflN3O5VA5m6I~ymtzGJfbvSFGEoVxSV5RAhDY5RBqgy15Ub4-OI499AqbfYzVh9iMxgIjCSgZtWHL882Vk4cEkKOUmIqEL7b4CjrhltbQ__,',
-  'https://s3-alpha-sig.figma.com/img/603a/9666/60b66f76b449e958ae4f1299284b4558?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=kxJq3ifxUwzCp2a0dPq00jX7tno7i34YgT0Ums-wlONymOsD-Z90tk9x-6MS7Ixy82o5usQNLZAX4~ZUsWGq7D1WMHBC7gdR5Z0j-CdBHbyJByWqMWJpVg5zhb1SHUkwqugjbpFEaQ9nRD1QRDzGDgFXfULsCX91olcGFycpmGB8cA0Ez4MsHnqAOVCqIr26lwbH5u-5WEqseIKXITNuOZ3Fsfgq-VJxLjN1kLf3dFgdeflN3O5VA5m6I~ymtzGJfbvSFGEoVxSV5RAhDY5RBqgy15Ub4-OI499AqbfYzVh9iMxgIjCSgZtWHL882Vk4cEkKOUmIqEL7b4CjrhltbQ__',
-]
+import image1 from '../../src/assets/imagemenures/image1.png'
+import image2 from '../../src/assets/imagemenures/image2.png'
+import image3 from '../../src/assets/imagemenures/image3.png'
+import image from '../../src/assets/imagemenures/image.png'
+
+export const images: string[] = [image, image1, image2, image3]
