790ce480f60aed5127a13f369bb3fc1c75dc993c
Mahdi <mahdibagherichanel@gmail.com>
2025-03-07 10:00:44 +0330

    Send phone number and receive code

--- Front/src/components/Login/ModalLogin.tsx
+++ Front/src/components/Login/ModalLogin.tsx
@@ -2,11 +2,17 @@ import { IoIosClose } from 'react-icons/io'
 import Logo from '../Logo/Logo'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useForm } from 'react-hook-form'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
+
 interface ModalProps {
   isMobile: boolean
   isModalOpen: boolean
   toggleModal: () => void
+}
+
+interface FormData {
+  mobile: string
+  otpCode: string
 }
 
 const Modal: React.FC<ModalProps> = ({
@@ -18,33 +24,81 @@ const Modal: React.FC<ModalProps> = ({
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<{ phone: string }>()
+  } = useForm<FormData>()
 
   const queryClient = useQueryClient()
 
   const loginMutation = useMutation({
-    mutationFn: async (data: { phone: string }) => {
-      const res = await axios.post(
-        'http://localhost:3000/api/v1/client/auth/SignIn',
-        data,
+    mutationFn: async (data: { mobile: string }): Promise<any> => {
+      const payload = JSON.stringify({ mobile: data.mobile })
+
+      const res = await fetch(
+        'http://localhost:3000/api/v1/client/auth/getOtp',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+          body: payload,
+        },
       )
-      return res.data
+
+      if (!res.ok) {
+        throw new Error('مشکلی پیش آمد')
+      }
+
+      const dataResponse = await res.json()
+      return dataResponse
     },
     onSuccess: (data) => {
       alert(`OTP ارسال شد: ${data.otp}`)
       queryClient.invalidateQueries(['getOtp'])
     },
-    onError: (error: any) => {
+    onError: (error: AxiosError) => {
+      console.log('🔴 خطای سرور:', error.message)
+      alert(`خطا: ${error.message || 'مشکلی پیش آمد'}`)
+    },
+  })
+
+  const otpVerificationMutation = useMutation({
+    mutationFn: async (data: {
+      mobile: string
+      otpCode: string
+    }): Promise<any> => {
+      const payload = { mobile: data.mobile, otpCode: data.otpCode }
+
+      console.log(payload)
+      const res = await axios.post(
+        'http://localhost:3000/api/v1/client/auth/SignIn',
+        payload,
+        {
+          headers: { 'Content-Type': 'application/json' },
+        },
+      )
+
+      return res.data
+    },
+    onSuccess: () => {
+      alert('ورود موفقیت‌آمیز بود.')
+      queryClient.invalidateQueries(['getOtp'])
+    },
+    onError: (error: AxiosError) => {
+      console.log('🔴 خطای سرور:', error.response?.data)
       alert(`خطا: ${error.response?.data?.message || 'مشکلی پیش آمد'}`)
     },
   })
 
-  const onSubmit = (data: { phone: string }) => {
-    loginMutation.mutate(data) // مقدار درست را ارسال کن
+  const onSubmit = (data: { mobile: string }) => {
+    loginMutation.mutate({ mobile: data.mobile })
+    console.log(data.mobile)
+  }
+
+  const onOtpSubmit = (data: { mobile: string; otpCode: string }) => {
+    otpVerificationMutation.mutate(data)
   }
 
   if (!isModalOpen) return null
-
   return (
     <div
       className={`fixed h-full w-full backdrop-blur-sm ${
@@ -55,34 +109,29 @@ const Modal: React.FC<ModalProps> = ({
     >
       <div className="relative w-[90%] max-w-[400px] transform rounded-lg bg-white p-8 shadow-lg transition-all duration-500">
         <h2 className="mb-6 text-center text-xl font-bold">مدال</h2>
-
         <div className="flex flex-col items-center text-center">
           <div className="mb-4 flex justify-center">
             <Logo />
           </div>
-
           <p className="mb-4 text-sm text-gray-600">
             با وارد کردن شماره موبایل کد تاییدی برای شما ارسال خواهد شد.
           </p>
-
-          {/* فرم ورود */}
           <form onSubmit={handleSubmit(onSubmit)} className="w-full">
             <input
               type="text"
               placeholder="شماره تلفن"
-              {...register('phone', {
+              {...register('mobile', {
                 required: 'شماره تلفن اجباری است',
                 pattern: {
-                  value: /^[0-9]{10,11}$/,
-                  message: 'شماره معتبر نیست',
+                  value: /^09[0-9]{9}$/,
+                  message: 'شماره تلفن باید با 091 شروع و ۱۰ رقم باشد',
                 },
               })}
               className="mb-2 w-full rounded-lg border border-gray-300 p-3 transition-all duration-300 focus:border-blue-500 focus:outline-none"
             />
-            {errors.phone && (
-              <p className="text-xs text-red-500">{errors.phone.message}</p>
+            {errors.mobile && (
+              <p className="text-xs text-red-500">{errors.mobile.message}</p>
             )}
-
             <button
               type="submit"
               className="mt-2 w-full rounded-lg bg-[#417F56] py-3 text-white transition-colors duration-300 hover:bg-[#365E42] focus:outline-none"
@@ -90,13 +139,36 @@ const Modal: React.FC<ModalProps> = ({
               ادامه
             </button>
           </form>
-
+          {loginMutation.isSuccess && (
+            <form onSubmit={handleSubmit(onOtpSubmit)} className="mt-4 w-full">
+              <input
+                type="text"
+                placeholder="کد تایید"
+                {...register('otpCode', {
+                  required: 'کد تایید اجباری است',
+                  minLength: {
+                    value: 5,
+                    message: 'کد تایید باید حداقل ۵ رقم باشد',
+                  },
+                })}
+                className="mb-2 w-full rounded-lg border border-gray-300 p-3 transition-all duration-300 focus:border-blue-500 focus:outline-none"
+              />
+              {errors.otpCode && (
+                <p className="text-xs text-red-500">{errors.otpCode.message}</p>
+              )}
+              <button
+                type="submit"
+                className="mt-2 w-full rounded-lg bg-[#417F56] py-3 text-white transition-colors duration-300 hover:bg-[#365E42] focus:outline-none"
+              >
+                تایید کد
+              </button>
+            </form>
+          )}
           <p className="mt-4 text-xs text-gray-500">
             ورود و عضویت در ترخینه به منزله{' '}
             <span className="text-[#417F56]">قبول قوانین و مقررات</span> است.
           </p>
         </div>
-
         <button
           onClick={toggleModal}
           className="absolute top-4 right-4 p-2 text-gray-500 transition-all duration-300 hover:text-gray-700"
