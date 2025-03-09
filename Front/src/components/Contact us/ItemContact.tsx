8b68699df5777599b0668bd51809cdf5d219388b
Mahdi <mahdibagherichanel@gmail.com>
2025-03-09 14:55:35 +0330

    Add hover effect to buttons to show on hover

--- Front/src/components/Contact us/ItemContact.tsx
+++ Front/src/components/Contact us/ItemContact.tsx
@@ -8,35 +8,35 @@ interface ContactProps {
 
 function ItemContact({ title, address, contactNumber, image }: ContactProps) {
   return (
-    <div className="flex flex-col items-center overflow-hidden rounded-lg border bg-white p-4 shadow-md md:flex-row">
-      <div className="w-full p-4 md:w-1/2">
-        <h2 className="text-center text-[20px] font-bold text-[#353535]">
+    <div className="flex h-[280px] w-[1200px] flex-col items-center overflow-hidden rounded-xl border border-[#CBCBCB] bg-white p-6 transition-all duration-300 hover:border-[#b0afaf] md:flex-row">
+      <div className="w-full p-6 md:w-1/2">
+        <h2 className="text-center text-xl font-semibold text-[#2D2D2D]">
           {title}
         </h2>
-        <p className="mt-2 text-center text-[18px] font-bold text-[#717171]">
-          آدرس : {address || 'آدرس ثبت نشده'}
+        <p className="mt-3 text-center text-lg font-medium text-[#555]">
+          آدرس: {address || 'آدرس ثبت نشده'}
         </p>
-        <p className="mt-1 text-center text-[18px] font-bold text-[#717171]">
-          شماره تماس : {contactNumber || 'شماره ثبت نشده'}
+        <p className="mt-2 text-center text-lg font-medium text-[#555]">
+          شماره تماس: {contactNumber || 'شماره ثبت نشده'}
         </p>
-        <p className="mt-1 text-center text-[18px] font-bold text-[#717171]">
+        <p className="mt-2 text-center text-lg font-medium text-[#555]">
           ساعت کاری : : همه‌روزه از ساعت ۱۲ تا ۲۳ بجز روز‌های تعطیل
         </p>
 
-        <div className="mt-4 flex justify-center gap-3">
-          <button className="hidden cursor-pointer rounded-lg border border-[#417F56] bg-[#417F56] px-4 py-2 text-sm font-semibold text-white transition hover:bg-white hover:text-black md:block">
+        <div className="mt-6 flex justify-center gap-4">
+          <button className="hidden cursor-pointer rounded-lg border border-[#417F56] bg-[#417F56] px-5 py-3 text-sm font-semibold text-white transition-all hover:bg-white hover:text-[#417F56] md:block">
             دیدن در نقشه
           </button>
-          <button className="hidden cursor-pointer rounded-lg border border-gray-300 bg-white px-4 py-2 text-sm font-semibold text-gray-700 transition hover:bg-[#417F56] hover:text-white md:block">
+          <button className="hidden cursor-pointer rounded-lg border border-gray-300 bg-white px-5 py-3 text-sm font-semibold text-gray-700 transition-all hover:bg-[#417F56] hover:text-white md:block">
             صفحه شعبه
           </button>
         </div>
       </div>
-      <div className="h-48 w-full overflow-hidden md:h-70 md:w-1/2">
+      <div className="h-48 w-full overflow-hidden rounded-xl md:h-80 md:w-150">
         <img
           src={image}
           alt={title}
-          className="h-full w-full rounded-lg object-cover"
+          className="h-full w-full rounded-xl object-cover transition-all duration-300 hover:scale-105"
         />
       </div>
     </div>
