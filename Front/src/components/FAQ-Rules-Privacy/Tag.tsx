4553c4b80741e21bc029605a9450807753b81170
Mahdi <mahdibagherichanel@gmail.com>
2025-03-12 18:00:59 +0330

    Finishing Footer Changes for Caber Review Section (Rules)

--- Front/src/components/FAQ-Rules-Privacy/Tag.tsx
+++ Front/src/components/FAQ-Rules-Privacy/Tag.tsx
@@ -1,28 +1,25 @@
-import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
-interface Props{
-  name:string
-}
+
 const Tag = () => {
-const location=useLocation()
-const path=location.pathname
-const links = [  
-  { to: '/faq', label: 'سوالات متداول' },  
-  { to: '/rules', label: 'قوانین' },  
-  { to: '/privacy', label: 'حریم خصوصی' }  
-];  
+  const location = useLocation()
+  const path = location.pathname
+  const links = [
+    { to: '/faq', label: 'سوالات متداول' },
+    { to: '/rules', label: 'قوانین' },
+    { to: '/privacy', label: 'حریم خصوصی' },
+  ]
 
   return (
-    <div className="w-full rounded-2xl flex gap-3 flex-row-reverse  bg-[#EDEDED] " >
-     {links.map((link) => (  
-                <Link  
-                    key={link.to}  
-                    className={`block py-4 mr-6 ${path === link.to ? ' border-b-2 border-green-700 text-green-700' : 'text-[#717171]'}`}  
-                    to={link.to}  
-                >  
-                    {link.label}  
-                </Link>  
-            ))}  
+    <div className="flex w-full flex-row-reverse gap-3 rounded-2xl bg-[#EDEDED]">
+      {links.map((link) => (
+        <Link
+          key={link.to}
+          className={`mr-6 block py-4 ${path === link.to ? 'border-b-2 border-green-700 text-green-700' : 'text-[#717171]'}`}
+          to={link.to}
+        >
+          {link.label}
+        </Link>
+      ))}
     </div>
   )
 }
