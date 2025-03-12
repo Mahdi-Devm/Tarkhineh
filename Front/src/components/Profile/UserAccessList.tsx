6b51538b001eaa27b894f2ffb0e0b3204cd90752
Mahdi <mahdibagherichanel@gmail.com>
2025-03-12 10:39:00 +0330

    Improve the code

--- Front/src/components/Profile/UserAccessList.tsx
+++ Front/src/components/Profile/UserAccessList.tsx
@@ -1,27 +1,33 @@
-import { CiUser } from 'react-icons/ci'
+import {
+  CiUser,
+  CiHeart,
+  CiLocationOn,
+  CiBoxList,
+  CiLogout,
+} from 'react-icons/ci'
+
 function UserAccessList() {
+  const menuItems = [
+    { title: 'پروفایل', icon: <CiUser /> },
+    { title: 'پیگیری سفارشات', icon: <CiBoxList /> },
+    { title: 'علاقمندی‌ها', icon: <CiHeart /> },
+    { title: 'آدرس‌های من', icon: <CiLocationOn /> },
+    { title: 'خروج', icon: <CiLogout /> },
+  ]
+
   return (
-    <div className="h-[206px] w-[272px] gap-y-2">
-      <div className="flex h-[38px] w-[264px] items-center justify-end">
-        <h1 className="text-[16px]">پروفایل</h1>
-        <CiUser className="h-[20px] w-[20px] text-[#417F56]" />
-      </div>
-      <div className="flex h-[38px] w-[264px] items-center justify-end">
-        <h1 className="text-[16px]">پیگیری سفارشات</h1>
-        <CiUser className="h-[20px] w-[20px] text-[#417F56]" />
-      </div>
-      <div className="flex h-[38px] w-[264px] items-center justify-end">
-        <h1 className="text-[16px]">علاقمندی‌ها</h1>
-        <CiUser className="h-[20px] w-[20px] text-[#417F56]" />
-      </div>
-      <div className="flex h-[38px] w-[264px] items-center justify-end">
-        <h1 className="text-[16px]">آدرس‌های من</h1>
-        <CiUser className="h-[20px] w-[20px] text-[#417F56]" />
-      </div>
-      <div className="flex h-[38px] w-[264px] items-center justify-end">
-        <h1 className="text-[16px]">خروج</h1>
-        <CiUser className="h-[20px] w-[20px] text-[#417F56]" />
-      </div>
+    <div className="mt-2 h-[206px] w-[272px] gap-y-2">
+      {menuItems.map((item, index) => (
+        <div
+          key={index}
+          className="flex h-[38px] w-[264px] cursor-pointer items-center justify-between rounded-md p-2 transition duration-300 hover:bg-[#E5F2E9]"
+        >
+          <h1 className="text-[16px]">{item.title}</h1>
+          <div className="h-[20px] w-[20px] text-[25px] text-[#417F56]">
+            {item.icon}
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
