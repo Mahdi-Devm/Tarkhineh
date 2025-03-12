a85aed7a4361dbf05cd7f0696efe1ad7a713289f
Mahdi <mahdibagherichanel@gmail.com>
2025-03-12 17:11:02 +0330

    Create an outline for interests before receiving the API from the back-end

--- Front/src/components/Profile/Interests.tsx
+++ Front/src/components/Profile/Interests.tsx
@@ -1,10 +1,29 @@
 import HeaderListProfile from './HeaderListProfile'
+
 const dataInterests = [
   { name: 'همه' },
-  { name: 'غذای های اصلی' },
-  { name: 'پیش غذا ' },
-  { name: 'دسر ' },
+  { name: 'غذاهای اصلی' },
+  { name: 'پیش‌غذا' },
+  { name: 'دسر' },
   { name: 'نوشیدنی' },
+]
+
+const fakeItems = [
+  {
+    name: 'پاستا سبزیجات',
+    price: '150,000',
+    img: 'https://www.figma.com/file/a1A9NqKpy0fC9pQpVBkTaa/image/117ab033c18324ae6672db4300937e223eb47955',
+  },
+  {
+    name: 'سالاد سزار',
+    price: '120,000',
+    img: 'https://www.figma.com/file/a1A9NqKpy0fC9pQpVBkTaa/image/117ab033c18324ae6672db4300937e223eb47955',
+  },
+  {
+    name: 'استیک گوشت',
+    price: '450,000',
+    img: 'https://www.figma.com/file/a1A9NqKpy0fC9pQpVBkTaa/image/117ab033c18324ae6672db4300937e223eb47955',
+  },
 ]
 
 function Interests() {
@@ -12,38 +31,42 @@ function Interests() {
     <div>
       <HeaderListProfile tilte="علاقمندی‌ها" />
       <div className="p-2">
-        <div className="flex items-center justify-center">
-          <div className="h-[41px] w-[351px]">
-            <input
-              className="h-[41px] w-[351px] rounded-2xl bg-stone-100 p-2"
-              type="جستجو"
-              placeholder="جستجو"
-            />
-          </div>
-          <div className="flex h-[32px] w-[470px] items-center justify-center gap-2">
+        <div className="flex flex-col items-center gap-4 md:flex-row md:justify-center">
+          <input
+            className="h-[41px] w-[80%] rounded-2xl bg-stone-100 p-2 md:w-[351px]"
+            type="text"
+            placeholder="جستجو"
+          />
+          <div className="flex flex-wrap items-center justify-center gap-2">
             {dataInterests.map((item) => (
-              <p className="rounded-2xl bg-[#EDEDED] p-2" key={item.name}>
+              <p
+                key={item.name}
+                className="cursor-pointer rounded-2xl bg-[#EDEDED] px-4 py-2 text-sm hover:bg-[#417F56] hover:text-white"
+              >
                 {item.name}
               </p>
             ))}
           </div>
         </div>
-        <div className="mt-5 h-[280px] w-[277px] rounded-2xl bg-amber-200 p-2">
-          <div>
-            <img
-              src="https://www.figma.com/file/a1A9NqKpy0fC9pQpVBkTaa/image/117ab033c18324ae6672db4300937e223eb47955"
-              alt=""
-            />
-          </div>
-          <div>
-            <p>@</p>
-            <p>پاستا سبزیجات </p>
-          </div>
-          <div>
-            <div></div>
-            <div>تومان 150,000</div>
-          </div>
-          <button className="h-[40px] w-[245px]">افزودن به سبد خرید</button>
+
+        <div className="mt-5 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+          {fakeItems.map((item, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center gap-2 rounded-2xl bg-white p-4 shadow-md transition hover:scale-105 hover:shadow-lg"
+            >
+              <img
+                src={item.img}
+                alt={item.name}
+                className="h-[180px] w-[300px] rounded-lg object-cover"
+              />
+              <h2 className="text-lg font-semibold">{item.name}</h2>
+              <p className="text-[#717171]">تومان {item.price}</p>
+              <button className="h-[40px] w-[80%] rounded-md bg-[#417F56] text-white hover:bg-[#315A3D]">
+                افزودن به سبد خرید
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </div>
