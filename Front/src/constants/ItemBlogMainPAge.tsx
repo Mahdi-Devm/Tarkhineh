292f0d35aeb9394c6a3bfc686bf184afb35797b7
Mahdi <mahdibagherichanel@gmail.com>
2025-03-17 13:16:38 +0330

    Improving the entire site photos

--- Front/src/constants/ItemBlogMainPAge.tsx
+++ Front/src/constants/ItemBlogMainPAge.tsx
@@ -1,7 +1,6 @@
-import Image from '../assets/optimized/a.png'
-import Image1 from '../assets/optimized/b.png'
-import Image2 from '../assets/optimized/c.png'
-import Image3 from '../assets/optimized/d.png'
+import Image from '../assets/optimized/8295be0e464709726a66931b63d8cfdb(1).jpg'
+import Image2 from '../assets/optimized/ecbd6208324f50f99155cc7c608e35e2(1).jpg'
+import Image3 from '../assets/optimized/d5c39ff5c7dcfd7412a964561eb2869d(1).jpg'
 export const branches = [
   {
     id: 1,
@@ -13,7 +12,7 @@ export const branches = [
     id: 2,
     name: 'شعبه اقدسیه',
     address: 'خیابان اقدسیه، نرسیده به میدان خیابان پلاک ۸',
-    image: Image1,
+    image: Image3,
   },
   {
     id: 3,
@@ -26,6 +25,6 @@ export const branches = [
     id: 4,
     name: 'شعبه اکباتان',
     address: 'شهنک اکباتان، فار۱۳ مجتمع تجاری کوروفل، طبقه سوم',
-    image: Image3,
+    image: Image2,
   },
 ]
