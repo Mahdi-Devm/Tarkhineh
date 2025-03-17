23cca20194ef28dbdb487bc361e4954af1a825b1
Mahdi <mahdibagherichanel@gmail.com>
2025-03-17 12:22:12 +0330

    Add bin photos for the project

--- Front/src/constants/ItemBlogMainPAge.tsx
+++ Front/src/constants/ItemBlogMainPAge.tsx
@@ -1,31 +1,31 @@
+import Image from '../assets/optimized/a.png'
+import Image1 from '../assets/optimized/b.png'
+import Image2 from '../assets/optimized/c.png'
+import Image3 from '../assets/optimized/d.png'
 export const branches = [
   {
     id: 1,
     name: 'شعبه ونگ',
     address: 'میدان ونگ، خیابان فردوسی، نیش کوچه نپوهر, پلاک ۲۶',
-    image:
-      'https://s3-alpha-sig.figma.com/img/91f3/86d4/8295be0e464709726a66931b63d8cfdb?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=cs9DknVq1ebwziYxgk6hO-ubugk76tavjY3b53D7yDv1BLo-71IcOpeLqtFZ5je~bG07LxGzFmv6wJyhLsQywXG7rhmieGqOCwy3qHLyHwJQ8KwkxMN4Y7-RUjc9tfYoXX1OfEoB8DbfISSmB~OdBqIzAGZq4bTdT1aomJXGPevow9aKF-1dpriu0kVnfplGlT7A20wZJd9FhF3e8f0ebOIXjKutIh8Bhjc2p09s79fw-10rLAjg6XXJtjkygj4bidjvtZhVLspdsZzB~3lZheAd0z-xjZpFMZLpSEr8Djw74J~TL7mp289KV2ijOQ4KQuDZx~J4sYTUlEVG3e1sNw__',
+    image: Image,
   },
   {
     id: 2,
     name: 'شعبه اقدسیه',
     address: 'خیابان اقدسیه، نرسیده به میدان خیابان پلاک ۸',
-    image:
-      'https://s3-alpha-sig.figma.com/img/4648/0497/d5c39ff5c7dcfd7412a964561eb2869d?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=O-fi4hhV8QAjOUQY99x4Jvtc9g6nH3i7rkOq8a6hZZS6F0mXJtwUT5NqdrcNaSDLXmN5G9661eGvNS-iHdfOKSIbL3KCJGYM7qXdSuJ0I5xEjZ1PowhcpiD4FxciS4NMPcn~QmJH0z24DzkPUS6FLKSA1QSdwZSCn6ZVL3cMT7Vs2esI1a1O7NIxhcoSdb-0p5cfMNdz94RYElhUt8SlGFXnUcu69ntoUovPDK4LPLVEVdCGAC-QQXAkO7fu7pwMxT2No8Doo8Aa1haM-vrm4dreQCioIdQOE~3hXH-o1zCLZXnukIeYjjt~zX84g4LVRIab5pAXTNziS8D~xzG4hw__',
+    image: Image1,
   },
   {
     id: 3,
     name: 'شعبه چالوس',
     address:
       'چالوس، خیابان ۷۱ شهرنور، بعد کوچه کوروفل، جنب داروخانه دکتر میانی',
-    image:
-      'https://s3-alpha-sig.figma.com/img/4648/0497/d5c39ff5c7dcfd7412a964561eb2869d?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=O-fi4hhV8QAjOUQY99x4Jvtc9g6nH3i7rkOq8a6hZZS6F0mXJtwUT5NqdrcNaSDLXmN5G9661eGvNS-iHdfOKSIbL3KCJGYM7qXdSuJ0I5xEjZ1PowhcpiD4FxciS4NMPcn~QmJH0z24DzkPUS6FLKSA1QSdwZSCn6ZVL3cMT7Vs2esI1a1O7NIxhcoSdb-0p5cfMNdz94RYElhUt8SlGFXnUcu69ntoUovPDK4LPLVEVdCGAC-QQXAkO7fu7pwMxT2No8Doo8Aa1haM-vrm4dreQCioIdQOE~3hXH-o1zCLZXnukIeYjjt~zX84g4LVRIab5pAXTNziS8D~xzG4hw__',
+    image: Image2,
   },
   {
     id: 4,
     name: 'شعبه اکباتان',
     address: 'شهنک اکباتان، فار۱۳ مجتمع تجاری کوروفل، طبقه سوم',
-    image:
-      'https://s3-alpha-sig.figma.com/img/304f/c307/284a13bf5484d9662967305806e58e8c?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=e2QVqNvn~e8-bygfYanbb39wetwu-MDmDZsUkPvnPnUkqr-vbICyqcw3wGVH54pIae~WeuFsPWEAtipwkaSd1kR1KRODcCBorUr3IoVSMV85dQ7zT7oZEaR5vPj4PtJnXDB2zrnpdH9zY2ASm8S6WMfyGmEZpdd9EUcYRANd6PV7p-ciH4GLsbeFfNvqMeJpfTyts9x33jlDfgFa3~W6sIkKtbNrt80GXUwq~8arsq~GiB~RntXhBbJtKpOUcPb1p8fysouvksdDy1KtY~-0QFPxATTry0Av8Z-~JCf5-6qv33MYo1Gsd-sCXm9FXmyNDwzDz3oqsT9y~vE0YOPiDA__',
+    image: Image3,
   },
 ]
