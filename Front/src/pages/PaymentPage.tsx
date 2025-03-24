e13a981d06b86f35a13bd767dc61e40d2f60e8e2
aliikh79 <a.khodabandeh90s@gmail.com>
2025-03-25 00:19:48 +0330

    separate userCoupon and add modal

--- Front/src/pages/PaymentPage.tsx
+++ Front/src/pages/PaymentPage.tsx
@@ -5,43 +5,12 @@ import OrderDetail from '../components/Basket/OrderDetail'
 import { useDispatch, useSelector } from 'react-redux'
 import { getPayLink, getPaymentOption } from '../redux/orderInfo/orderInfoSlice'
 import { RootState } from '../redux/store'
-import Cookies from 'js-cookie'
-import { FormEvent, useState } from 'react'
-import { useCoupon } from '../redux/shopCard/shopCardSlice'
-import { toast } from 'react-toastify'
+import UserCoupon from '../components/Basket/UserCoupon'
 
 function PaymentPage() {
-  const { amount } = useSelector((state: RootState) => state.cardReducer)
-  const [addedCoupon, setAddedCoupon] = useState('')
+  
   const dispatch = useDispatch()
-  const validateCoupon = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    console.log(addedCoupon)
-    const token = Cookies.get('accessToken')
-    const res = await fetch(
-      'http://localhost:3000/api/v1/client/userCoupon/use',
-      {
-        method: 'POST',
-        headers: {
-          'Content-type':'application/json',
-          
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({
-          coupon_code: addedCoupon,
-          totalPrice:amount          
-        }),
-      },
-    )
-    
-    const data=await res.json()
-  if(res.status!==201) toast.error('کد نامعتبر است')
-    
-  else  toast.success('تخفیف اعمال شد ')
-const {coupon}=data    
-
-    dispatch(useCoupon(coupon))
-  }
+  
 
   const { pathname } = useLocation()
 
@@ -62,22 +31,7 @@ const {coupon}=data
       <PaymentBreadcrumb pathName={pathname} />
       <main className="mx-auto my-6 flex max-w-[1224px] flex-col gap-3 rounded-md text-[#353535] md:h-[554px] md:flex-row-reverse">
         <div className="flex w-full flex-col space-y-3 rounded-xl text-[#353535] sm:h-full">
-          <div className="flex flex-row-reverse flex-wrap items-center justify-between rounded-lg border border-zinc-300 p-4 text-right">
-            <p className="mb-2 text-right text-[20px] font-semibold">
-              ثبت کد تخفیف
-            </p>
-            <form onSubmit={(e) => validateCoupon(e)} className="flex gap-3">
-              <button className="h-full rounded-lg bg-green-800 px-6 py-2 text-nowrap text-white">
-                ثبت کد
-              </button>
-              <input
-                onChange={(e) => setAddedCoupon(e.target.value)}
-                className="w-full rounded-lg border border-zinc-400 px-3 placeholder:text-right"
-                placeholder="کد تخفیف"
-                type="text"
-              />
-            </form>
-          </div>
+          <UserCoupon/>
           <div className="flex flex-row-reverse items-center justify-between rounded-md border border-zinc-400 p-4 max-md:flex-wrap">
             <p className="mb-2 border-zinc-300 py-2 text-right max-md:w-full max-md:border-b">
               روش پرداخت
